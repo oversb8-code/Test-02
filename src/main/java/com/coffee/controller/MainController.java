1c240137632f9e1d9f719fc737646f0a0c37d289
oversb8@gmail.com <oversb8@gmail.com>
2026-05-11 19:06:48 +0900

    aaaaaaaaasssaasasafgf  ssss

--- src/main/java/com/coffee/controller/MainController.java
+++ src/main/java/com/coffee/controller/MainController.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MainController {
 
-    @GetMapping("/ ")
+    @GetMapping("/")
     public String hello() {
         return "Hello GitHub Actions!";
     }
